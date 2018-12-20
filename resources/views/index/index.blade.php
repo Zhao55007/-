feeb3b9204550f56083fe0349235bbf562aefd70
Zhao55007 <Zhao55006@163.com>
2018-12-20 19:32:31 +0800

    master aaaaa

---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>AAAAAAAAAAA</h1>
+	<h2>BBBBBBBBBBBBBBBB</h2>
+	<h3>CCCCCCCCCCCCCCCCCCCC</h3>
+	<h4>DDDDDDDDDDDDDDDDDDDDDDDDD</h4>
+
+
+</body>
+</html>
